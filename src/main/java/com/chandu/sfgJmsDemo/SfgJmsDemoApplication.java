d669abde01957f24992245a139cb2b14bfacbc32
chandu <chandu3015@gmail.com>
2020-01-02 22:50:47 -0600

    Initial Project Creation

---
+++ src/main/java/com/chandu/sfgJmsDemo/SfgJmsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.chandu.sfgJmsDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgJmsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgJmsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chandu/sfgJmsDemo/SfgJmsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chandu.sfgJmsDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgJmsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
